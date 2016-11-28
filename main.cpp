8ca98960a6813038f87904bbc1d82d7730604078
Zaki Petrov <Zaki Petrov>
2016-11-27 23:13:38 -0800

    Adding some stuff about the game

--- main.cpp
+++ main.cpp
@@ -7,32 +7,75 @@ using std::cin;
 #define BOARD_SIZE 3
 #define PLAYER_ONE 'X'
 #define PLAYER_TWO 'O'
-#define PLAYER_ONE_MOVE 1
-#define PLAYER_TWO_MOVE 2
+#define PLAYER_ONE_WON 1
+#define PLAYER_TWO_WON 2
 
-bool isActiveGame(char **board){
+bool isActiveGame(char board[BOARD_SIZE][BOARD_SIZE]){
 	//TODO
+	return true;
 }
 
-int whoWonTheGame(char **board){
+int whoWonTheGame(char board[BOARD_SIZE][BOARD_SIZE]){
 	//TODO
+	return 0;
 }
 
-int main(){
-	char board[BOARD_SIZE][BOARD_SIZE] = { { '_', '_', '_' }, { '_', '_', '_' }, { '_', '_', '_' } }; // Initialize the board 3x3
-	bool activeGame = true; 
-	unsigned short currentPlayerTurn = PLAYER_ONE_MOVE; // Current player turn
-	unsigned short posX = 0, posY = 0;  // Positions to put symbol to
+bool canPlaceThere(char board[BOARD_SIZE][BOARD_SIZE], unsigned short posX, unsigned short posY){
+	return board[posX][posY] != PLAYER_ONE && board[posX][posY] != PLAYER_TWO && (posX >= 0 && posX < BOARD_SIZE) && (posY >= 0 && posY < BOARD_SIZE);
+}
+
+void printBoard(char board[BOARD_SIZE][BOARD_SIZE]){
 	for (int i = 0; i < BOARD_SIZE; i++){
 		for (int j = 0; j < BOARD_SIZE; j++){
 			cout << board[i][j];
 		}
 		cout << endl;
 	}
-
+}
+int main(){
+	char board[BOARD_SIZE][BOARD_SIZE] = { { '_', '_', '_' }, { '_', '_', '_' }, { '_', '_', '_' } }; // Initialize the board 3x3
+	bool activeGame = true;
+	unsigned short posX = 0, posY = 0;  // Positions to put symbol to
+	bool playerOneMove = true;
+	bool playerTwoMove = false;
+	printBoard(board);
 	while (activeGame){
-		//TODO
+		if (playerOneMove){
+			cout << "Player one's turn!" << endl;
+			do{
+				cout << "Please enter position to put " << PLAYER_ONE << " sign!" << endl;
+				cin >> posX >> posY;
+			} while (!canPlaceThere(board, posX, posY));
+			board[posX][posY] = PLAYER_ONE;
+		}
+		if (playerTwoMove){
+			cout << "Player two's turn!" << endl;
+			do{
+				cout << "Please enter position to put " << PLAYER_TWO << " sign!" << endl;
+				cin >> posX >> posY;
+			} while (!canPlaceThere(board, posX, posY));
+			board[posX][posY] = PLAYER_TWO;
+		}
+		system("cls"); //clearing the screen
+		printBoard(board);
+		activeGame = isActiveGame(board);
+		if (!activeGame){
+			if (whoWonTheGame(board) == PLAYER_ONE_WON){
+				cout << "Player one won the game..!" << endl;
+			}
+			else{
+				cout << "Player two won the game..!" << endl;
+			}
+		}
+		if (playerOneMove){
+			playerOneMove = false;
+			playerTwoMove = true;
+			continue;
+		}
+		if (playerTwoMove){
+			playerTwoMove = false;
+			playerOneMove = true;
+		}
 	}
-	system("pause");
 	return 0;
 }
