0e142accf19846d1135efdf6ecbcc09a0f571e5c
zaki1993 <zaki1993@abv.bg>
2016-11-30 00:40:29 -0800

    Adding some more methods

--- version_java/Main.java
+++ version_java/Main.java
@@ -1,16 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
-	public static boolean isActiveGame(char [][]board,final byte BOARD_SIZE){
+	public static boolean isActiveGame(char [][]board){
 		//TODO
 		return true;
 	}
 
-	public static int whoWonTheGame(char [][]board,final byte BOARD_SIZE){
+	public static int whoWonTheGame(char [][]board){
 		//TODO
 		return 0;
 	}
-
+	
+	public static boolean hasFreeSpace(char [][]board){
+		for(int i = 0;i<board[0].length;i++){
+			for(int j = 0;j<board[1].length;j++){
+				if(board[i][j] == '_'){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isWin(char [][]board, final char PLAYER_ONE, final char PLAYER_TWO, final char[][][] WIN_POSITIONS){
+		for(int i = 0;i<board[0].length;i++){
+			for(int j = 0;j<board[1].length;j++){
+				//TODO
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isDraw(char [][]board, final char PLAYER_ONE, final char PLAYER_TWO, final char[][][] WIN_POSITIONS){
+		return !isWin(board, PLAYER_ONE, PLAYER_TWO, WIN_POSITIONS) && !hasFreeSpace(board);
+	}
+	
 	public static boolean canPlaceThere(char [][]board, byte posX, byte posY,final byte BOARD_SIZE, final char PLAYER_ONE, final char PLAYER_TWO){
 		return board[posX][posY] != PLAYER_ONE && board[posX][posY] != PLAYER_TWO && (posX >= 0 && posX < BOARD_SIZE) && (posY >= 0 && posY < BOARD_SIZE);
 	}
@@ -26,9 +50,15 @@ public class Main {
 	public static void main(String[] args) {
 			Scanner input = new Scanner(System.in);
 			char board[][] = { { '_', '_', '_' }, { '_', '_', '_' }, { '_', '_', '_' } }; // Initialize the board 3x3
-			final int[][][] DRAW_POSITION = {}; //TODO
-			final int[][][] WIN_POSITIONS = {}; //TODO
-		
+			final int [][][]WIN_POSITIONS = {{{0,0},{0,1},{0,2}},
+											{{1,0},{1,1},{1,2}},
+											{{2,0},{2,1},{2,2}},
+											{{0,0},{1,0},{2,0}},
+											{{0,1},{1,1},{2,1}},
+											{{0,2},{1,2},{2,2}},
+											{{0,0},{1,1},{2,2}},
+											{{0,2},{1,1},{2,0}}};
+			
 			byte posX = 0, posY = 0;  // Positions to put symbol to
 			
 			final byte BOARD_SIZE = 3; //Initialize board size
@@ -65,9 +95,9 @@ public class Main {
 				}
 				
 				printBoard(board, BOARD_SIZE);
-				activeGame = isActiveGame(board, BOARD_SIZE);
+				activeGame = isActiveGame(board);
 				if (!activeGame){
-					if (whoWonTheGame(board, BOARD_SIZE) == PLAYER_ONE_WON){
+					if (whoWonTheGame(board) == PLAYER_ONE_WON){
 						System.out.println("Player one won the game..!");
 					}
 					else{
