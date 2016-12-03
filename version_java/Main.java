ebe1de689626e480a06477860a8a9d9f59c28087
zaki1993 <zaki1993@abv.bg>
2016-12-02 17:21:27 -0800

    Almost DONE, one last method

--- version_java/Main.java
+++ version_java/Main.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-	public static boolean isActiveGame(char [][]board){
-		//TODO
-		return true;
+	public static boolean isActiveGame(char [][]board, final char PLAYER, final char BOT, final int[][][] WIN_POSITIONS){
+		return !isWinPlayer(board, PLAYER, WIN_POSITIONS) || !isWinBot(board, BOT, WIN_POSITIONS) || hasFreeSpace(board);
 	}
 
-	public static int whoWonTheGame(char [][]board){
-		//TODO
+	public static int whoWonTheGame(char [][]board, final char PLAYER, final char BOT, final int[][][] WIN_POSITIONS){
+		if(isWinPlayer(board, PLAYER, WIN_POSITIONS)){
+			return 1;
+		}
+		if(isWinBot(board, BOT, WIN_POSITIONS)){
+			return 2;
+		}
 		return 0;
 	}
 	
@@ -22,31 +26,50 @@ public class Main {
 		return false;
 	}
 	
-	public static boolean isWin(char [][]board, final char PLAYER_ONE, final char PLAYER_TWO, final char[][][] WIN_POSITIONS){
-		for(int i = 0;i<board[0].length;i++){
-			for(int j = 0;j<board[1].length;j++){
-				//TODO
+	public static boolean isWinPlayer(char [][]board, final char PLAYER, final int[][][] WIN_POSITIONS){
+		boolean hasWinner = true;
+		for(int i = 0;i<WIN_POSITIONS.length;i++){
+			hasWinner = true;
+			for(int j = 0;j<WIN_POSITIONS[i].length;j++){
+				if(board[WIN_POSITIONS[i][j][0]][WIN_POSITIONS[i][j][1]] != PLAYER){
+					hasWinner = false;
+					break;
+				}
 			}
 		}
-		return true;
+		return hasWinner;
+	}
+	public static boolean isWinBot(char [][]board, final char BOT, final int[][][] WIN_POSITIONS){
+		boolean hasWinner = true;
+		for(int i = 0;i<WIN_POSITIONS.length;i++){
+			hasWinner = true;
+			for(int j = 0;j<WIN_POSITIONS[i].length;j++){
+				if(board[WIN_POSITIONS[i][j][0]][WIN_POSITIONS[i][j][1]] != BOT){
+					hasWinner = false;
+					break;
+				}
+			}
+		}
+		return hasWinner;
 	}
 	
-	public static boolean isDraw(char [][]board, final char PLAYER_ONE, final char PLAYER_TWO, final char[][][] WIN_POSITIONS){
-		return !isWin(board, PLAYER_ONE, PLAYER_TWO, WIN_POSITIONS) && !hasFreeSpace(board);
-	}
-	
-	public static boolean canPlaceThere(char [][]board, byte posX, byte posY,final byte BOARD_SIZE, final char PLAYER_ONE, final char PLAYER_TWO){
-		return board[posX][posY] != PLAYER_ONE && board[posX][posY] != PLAYER_TWO && (posX >= 0 && posX < BOARD_SIZE) && (posY >= 0 && posY < BOARD_SIZE);
+	public static boolean canPlaceThere(char [][]board, byte posX, byte posY,final byte BOARD_SIZE, final char PLAYER, final char BOT){
+		return board[posX][posY] != PLAYER && board[posX][posY] != BOT && (posX >= 0 && posX < BOARD_SIZE) && (posY >= 0 && posY < BOARD_SIZE);
 	}
 
-	public static void printBoard(char [][]board, final byte BOARD_SIZE){
-		for (int i = 0; i < BOARD_SIZE; i++){
-			for (int j = 0; j < BOARD_SIZE; j++){
+	public static void printBoard(char [][]board){
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[i].length; j++){
 				System.out.print(board[i][j]);
 			}
 			System.out.println();
 		}
 	}
+	
+	public static void botMove(char [][]board, final char BOT){
+		//TODO
+	}
+	
 	public static void main(String[] args) {
 			Scanner input = new Scanner(System.in);
 			char board[][] = { { '_', '_', '_' }, { '_', '_', '_' }, { '_', '_', '_' } }; // Initialize the board 3x3
@@ -62,56 +85,58 @@ public class Main {
 			byte posX = 0, posY = 0;  // Positions to put symbol to
 			
 			final byte BOARD_SIZE = 3; //Initialize board size
-			final char PLAYER_ONE = 'X'; //Player one symbol
-			final char PLAYER_TWO = 'O'; //Player two symbol
-			final byte PLAYER_ONE_WON = 1;
-			final byte PLAYER_TWO_WON = 2;
+			final char PLAYER = 'X'; //Player symbol
+			final char BOT = 'O'; // Bot symbol
+			
+			final byte PLAYER_WON = 1; //Player win number
+			final byte BOT_WON = 2; //Bot win number
+			final byte DRAW = 0; //Draw number
 			
 			boolean activeGame = true;
-			boolean playerOneMove = true;
-			boolean playerTwoMove = false;
+			boolean playerMove = true;
+			boolean botMove = false;
 			
-			printBoard(board, BOARD_SIZE);
+			printBoard(board);
 			
 			//main game loop
 			while (activeGame){
-				if (playerOneMove){
+				if (playerMove){
 					System.out.println("Player one's turn!");
 					do{
-						System.out.println("Please enter position to put " + PLAYER_ONE + " sign!");
+						System.out.println("Please enter position to put " + PLAYER + " sign!");
 						posX = input.nextByte();
 						posY = input.nextByte();
-					}while (!canPlaceThere(board, posX, posY,BOARD_SIZE,PLAYER_ONE,PLAYER_TWO));
-					board[posX][posY] = PLAYER_ONE;
+					}while (!canPlaceThere(board, posX, posY,BOARD_SIZE,PLAYER,BOT));
+					board[posX][posY] = PLAYER;
 				}
-				if (playerTwoMove){
-					System.out.println("Player two's turn!");
-					do{
-						System.out.println("Please enter position to put " + PLAYER_TWO + " sign!");
-						posX = input.nextByte();
-						posY = input.nextByte();
-					}while (!canPlaceThere(board, posX, posY,BOARD_SIZE,PLAYER_ONE,PLAYER_TWO));
-					board[posX][posY] = PLAYER_TWO;
+				if (botMove){
+					botMove(board, BOT);
 				}
 				
-				printBoard(board, BOARD_SIZE);
-				activeGame = isActiveGame(board);
+				printBoard(board);
+				activeGame = isActiveGame(board, PLAYER, BOT, WIN_POSITIONS);
+				
 				if (!activeGame){
-					if (whoWonTheGame(board) == PLAYER_ONE_WON){
+					if (whoWonTheGame(board, PLAYER, BOT, WIN_POSITIONS) == PLAYER_WON){
 						System.out.println("Player one won the game..!");
 					}
-					else{
-						System.out.println("Player two won the game..!");
+					else if(whoWonTheGame(board, PLAYER, BOT, WIN_POSITIONS) == BOT_WON){
+						System.out.println("The bot won the game..!");
 					}
-				}
-				if (playerOneMove){
-					playerOneMove = false;
-					playerTwoMove = true;
+					else if(whoWonTheGame(board, PLAYER, BOT, WIN_POSITIONS) == DRAW){
+						System.out.println("It's draw");
+					}
 					continue;
 				}
-				if (playerTwoMove){
-					playerTwoMove = false;
-					playerOneMove = true;
+				
+				if (playerMove){
+					playerMove = false;
+					botMove = true;
+					continue;
+				}
+				if (botMove){
+					botMove = false;
+					playerMove = true;
 			}
 		}
 	}
